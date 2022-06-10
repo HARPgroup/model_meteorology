c1eee1a7d0e855cdd0a03d38fae93a44d94b9924
Robert W. Burgholzer <rburghol@vt.edu>
2022-06-10 21:04:10 +0000

    added standard R scripts

---
+++ R/get_lseg_summary_stats.R
@@ -0,0 +1,51 @@
+##### This script dowloads temp, precip, and pet data and creates/downloads csv of summary stats
+## Last Updated 4/12/21
+## HARP Group
+
+# load packages
+.libPaths("/var/www/R/x86_64-pc-linux-gnu-library/")
+#library(lubridate)
+library(sqldf)
+library(IHA)
+library(zoo)
+
+# load vahydro functions
+site <- "http://deq1.bse.vt.edu:81/d.dh"  #Specify the site of interest, either d.bet OR d.dh
+basepath <- '/var/www/R';
+source(paste(basepath,'config.R',sep='/'))
+
+# load lseg_functions
+source(paste(github_location,"HARParchive/HARP-2021-2022","lseg_functions.R", sep = "/"))
+# load landseg list of interest
+AllLandsegList <- scan(file = "https://raw.githubusercontent.com/HARPgroup/HARParchive/master/GIS_layers/p5_landsegments.txt", what = character())
+
+i <- 1
+while(i<=length(AllLandsegList)){
+  landseg <- AllLandsegList[i]
+  # read in land segment temperature and precipitation data
+  #dfPRC <- read.table(paste0("http://deq1.bse.vt.edu:81/met/out/lseg_csv/1984010100-2020123123/",landseg,".PRC"), header = FALSE, sep = ",")
+  #dfTMP <- read.table(paste0("http://deq1.bse.vt.edu:81/met/out/lseg_csv/1984010100-2020123123/",landseg,".TMP"), header = FALSE, sep = ",")
+  #dfPET <- read.table(paste0("http://deq1.bse.vt.edu:81/met/out/lseg_csv/1984010100-2020123123/",landseg,".PET"), header = FALSE, sep = ",")
+  dfPRC <- read.table(paste0("/backup/meteorology/out/lseg_csv/1984010100-2020123123/",landseg,".PRC"), header = FALSE, sep = ",")
+  dfTMP <- read.table(paste0("/backup/meteorology/out/lseg_csv/1984010100-2020123123/",landseg,".TMP"), header = FALSE, sep = ",")
+  dfPET <- read.table(paste0("/backup/meteorology/out/lseg_csv/1984010100-2020123123/",landseg,".PET"), header = FALSE, sep = ",")
+  colnames(dfTMP) = c("year","month","day","hour","temp")
+  dfTMP$date <- as.Date(paste(dfTMP$year,dfTMP$month,dfTMP$day, sep="-"))
+  colnames(dfPRC) = c("year","month","day","hour","precip")
+  dfPRC$date <- as.Date(paste(dfPRC$year,dfPRC$month,dfPRC$day, sep="-"))
+  colnames(dfPET) = c("year","month","day","hour","pet")
+  dfPET$date <- as.Date(paste(dfPET$year,dfPET$month,dfPET$day, sep="-"))
+  
+  # run get_lseg_summary_stats function
+  summaryStats <- get_lseg_summary_stats(dfTMP=dfTMP,dfPRC=dfPRC,dfPET=dfPET)
+  
+  # create and save PET file as csv
+  #write.table(summaryStats,paste0("C:/Users/alexw/Documents/R/HARP/Summer 2021/landsegPETfiles/",landseg,"SummaryStats.csv"), 
+  #            row.names = FALSE, col.names = TRUE, sep = ",")
+  write.table(summaryStats,paste0("/backup/meteorology/out/lseg_csv/1984010100-2020123123/",landseg,"SummaryStats.csv"), 
+             row.names = FALSE, col.names = TRUE, sep = ",")
+  
+  i<-i+1
+}
+
+
---
+++ R/lseg_qa_test_timeseries.R
@@ -0,0 +1,166 @@
+##### This script runs QA on land segment summary stat data. It generates a txt file with list of land segments it flags.
+## Last Updated 4/20/21
+## HARP Group
+## To change metric and QA testing value alter lines 33 and 45
+## Change the .XXX label at the end of the paste statement in line 36 to correspond to metric (ex: PRC = precipitation)
+## Change the numeric condition in the if statement to fit needs in line 45 (for precipitation > 1 corresponds to > 1 in/hr)
+
+# load packages and vahydro functions
+#.libPaths("/var/www/R/x86_64-pc-linux-gnu-library/")
+site <- "http://deq1.bse.vt.edu:81/d.dh"  #Specify the site of interest, either d.bet OR d.dh
+basepath <- '/var/www/R';
+source(paste(basepath,'config.R',sep='/'))
+
+library(lubridate)
+library(sqldf)
+library(IHA)
+library(zoo)
+library(data.table)
+
+# load lseg_functions
+site <- "http://deq1.bse.vt.edu:81/met/out/lseg_csv/1984010100-2020123123/" # temporary cloud url
+dir <- "/backup/meteorology/" # directory where met data is stored
+#source(paste(github_location,"HARParchive/HARP-2021-2022","lseg_functions.R", sep = "/"))
+
+# load AllLandsegList
+#AllLandsegList <- scan(file = "https://raw.githubusercontent.com/HARPgroup/HARParchive/master/GIS_layers/p5_landsegments.txt", what = character())
+AllLandsegList <- scan(file = paste0(dir, "p5_landsegments.txt"), what = character())
+paste("loaded land segment list")
+
+# instantiate data frames and variables for loops
+i <- 1
+ErrorLsegs <- data.frame()
+ErrorYear <- data.frame()
+ErrorMonth <- data.frame()
+ErrorDay <- data.frame()
+ErrorHour <- data.frame()
+ErrorValue <- data.frame()
+paste("data frames instantiated")
+while(i<=length(AllLandsegList)){
+  
+  landseg <- AllLandsegList[i]
+  print(paste0("current landsegment: ", landseg))
+
+  # read in lseg_csv
+  #timeSeries <- fread(paste0(site,landseg,".PRC"))
+  # code with correct input directory if running on deq machine
+  timeSeries <- fread(paste0(dir, "out/lseg_csv/1984010100-2020123123/",landseg,".PRC"))
+  #timeSeries <- read.table(paste0(dir, "out/lseg_csv/1984010100-2020123123/",landseg,".PRC"), header = FALSE, sep = ",")
+  print(paste0(landseg," data read"))
+
+  # line of code to help run even with incomplete lseg_csv
+  timeSeries <- timeSeries[-nrow(timeSeries),]
+  
+  # loops iterates through to check for abnormally values 
+  j <- 1
+  while (j <= nrow(timeSeries)) {
+    
+    if (as.numeric(timeSeries$V5[j]) > 4.0) {
+      ErrorLsegs <- rbind(ErrorLsegs, paste0(landseg))
+      ErrorYear <- rbind(ErrorYear, paste0(timeSeries$V1[j]))
+      ErrorMonth <- rbind(ErrorMonth, paste0(timeSeries$V2[j]))
+      ErrorDay <- rbind(ErrorDay, paste0(timeSeries$V3[j]))
+      ErrorHour <- rbind(ErrorHour, paste0(timeSeries$V4[j]))
+      ErrorValue <- rbind(ErrorValue, paste0(timeSeries$V5[j]))
+      print(paste0(landseg, " has an abnormality value"))
+    }
+    j <- j + 1
+  }
+  print(paste0(landseg, " abnormallity values checked"))
+  i <- i+ 1
+}
+
+# creates data framed of flagged values with corresponding time and land segment
+FlaggedData <- data.frame(ErrorYear, ErrorMonth, ErrorDay, ErrorHour, ErrorLsegs, ErrorValue)
+paste("Flagged data frame created")
+
+# create and save error landsegment file as txt
+#write.table(FlaggedData,"C:/Users/kylew/Documents/R/HARPSpring2021/NLDAS-2/p5FlaggedLsegsPRC.txt", 
+#            row.names = FALSE, col.names = FALSE)
+# code to write it to /backup/meteorology directory if we ever decide to
+write.table(FlaggedData,paste0(dir, "p5FlaggedLsegsPRC.txt"), 
+           row.names = FALSE, col.names = FALSE, sep = ",")
+paste("Data written to table")
+##### This script runs QA on land segment summary stat data. It generates a txt file with list of land segments it flags.
+## Last Updated 4/26/22
+## HARP Group
+## To change metric and QA testing value alter lines 33 and 45
+## Change the .XXX label at the end of the paste statement in line 36 to correspond to metric (ex: PRC = precipitation)
+## Change the numeric condition in the if statement to fit needs in line 45 (for precipitation > 1 corresponds to > 1 in/hr)
+
+# load packages and vahydro functions
+#.libPaths("/var/www/R/x86_64-pc-linux-gnu-library/")
+site <- "http://deq1.bse.vt.edu:81/d.dh"  #Specify the site of interest, either d.bet OR d.dh
+basepath <- '/var/www/R';
+source(paste(basepath,'config.R',sep='/'))
+
+library(lubridate)
+library(sqldf)
+library(IHA)
+library(zoo)
+library(data.table)
+
+# load lseg_functions
+site <- "http://deq1.bse.vt.edu:81/met/out/lseg_csv/1984010100-2020123123/" # temporary cloud url
+dir <- "/backup/meteorology/" # directory where met data is stored
+#source(paste(github_location,"HARParchive/HARP-2021-2022","lseg_functions.R", sep = "/"))
+
+# load AllLandsegList
+#AllLandsegList <- scan(file = "https://raw.githubusercontent.com/HARPgroup/HARParchive/master/GIS_layers/p5_landsegments.txt", what = character())
+AllLandsegList <- scan(file = paste0(dir, "p5_landsegments.txt"), what = character())
+paste("loaded land segment list")
+
+# instantiate data frames and variables for loops
+i <- 1
+ErrorLsegs <- data.frame()
+ErrorYear <- data.frame()
+ErrorMonth <- data.frame()
+ErrorDay <- data.frame()
+ErrorHour <- data.frame()
+ErrorValue <- data.frame()
+paste("data frames instantiated")
+while(i<=length(AllLandsegList)){
+  
+  landseg <- AllLandsegList[i]
+  print(paste0("current landsegment: ", landseg))
+
+  # read in lseg_csv
+  #timeSeries <- fread(paste0(site,landseg,".PRC"))
+  # code with correct input directory if running on deq machine
+  timeSeries <- fread(paste0(dir, "out/lseg_csv/1984010100-2020123123/",landseg,".PRC"))
+  #timeSeries <- read.table(paste0(dir, "out/lseg_csv/1984010100-2020123123/",landseg,".PRC"), header = FALSE, sep = ",")
+  print(paste0(landseg," data read"))
+
+  # line of code to help run even with incomplete lseg_csv
+  timeSeries <- timeSeries[-nrow(timeSeries),]
+  
+  # loops iterates through to check for abnormally values 
+  j <- 1
+  while (j <= nrow(timeSeries)) {
+    
+    if (as.numeric(timeSeries$V5[j]) > 4.0) {
+      ErrorLsegs <- rbind(ErrorLsegs, paste0(landseg))
+      ErrorYear <- rbind(ErrorYear, paste0(timeSeries$V1[j]))
+      ErrorMonth <- rbind(ErrorMonth, paste0(timeSeries$V2[j]))
+      ErrorDay <- rbind(ErrorDay, paste0(timeSeries$V3[j]))
+      ErrorHour <- rbind(ErrorHour, paste0(timeSeries$V4[j]))
+      ErrorValue <- rbind(ErrorValue, paste0(timeSeries$V5[j]))
+      print(paste0(landseg, " has an abnormality value"))
+    }
+    j <- j + 1
+  }
+  print(paste0(landseg, " abnormallity values checked"))
+  i <- i+ 1
+}
+
+# creates data framed of flagged values with corresponding time and land segment
+FlaggedData <- data.frame(ErrorYear, ErrorMonth, ErrorDay, ErrorHour, ErrorLsegs, ErrorValue)
+paste("Flagged data frame created")
+
+# create and save error landsegment file as txt
+#write.table(FlaggedData,"C:/Users/kylew/Documents/R/HARPSpring2021/NLDAS-2/p5FlaggedLsegsPRC.txt", 
+#            row.names = FALSE, col.names = FALSE)
+# code to write it to /backup/meteorology directory if we ever decide to
+write.table(FlaggedData,paste0(dir, "p5FlaggedLsegsPRC.txt"), 
+           row.names = FALSE, col.names = FALSE, sep = ",")
+paste("Data written to table")
