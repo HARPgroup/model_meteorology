689579c41294d45a6ee3f1ac0e14db902488c9bb
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2023-09-12 08:21:03 -0400

    fix

--- R/time_template.R
+++ R/time_template.R
@@ -1,4 +1,5 @@
 library(sqldf)
+source("https://raw.githubusercontent.com/HARPgroup/model_meteorology/main/R/synthetic_met_functions.R")
 
 # match dates in a time series
 
@@ -14,7 +15,7 @@ landseg <- as.character(argst[1])
 in_dir <- as.character(argst[2]) 
 time_template <- as.character(argst[3]) 
 
-time_template <- fread(wdm_template)
+time_template <- data.table::fread(wdm_template)
 #./make_wdm_template 1984 2024 met 1000
 
 fexts <- s('PRC', 'PET', 'TMP', 'RAD', 'DPT', 'WND')
