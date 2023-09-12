38d7f50be92ef9619819030a53ec9751ffabf1c0
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2023-09-12 07:43:10 -0400

    draft template handling script

--- R/time_template.R
+++ R/time_template.R
@@ -4,13 +4,24 @@ library(sqldf)
 
 # get inputs
 argst <- commandArgs(trailingOnly=T)
-if (length(argst) < 5) {
-  message("Use: Rscript time_template.R met_ts time_template")
-  message("Ex: Rscript time_template.R /backup/meteorology/out/lseg_csv/mash/N51187.PET /tmp/met_1000.csv")
+if (length(argst) < 3) {
+  message("Use: Rscript time_template.R landseg in_dir time_template")
+  message("Ex: Rscript time_template.R N51011 /backup/meteorology/out/lseg_csv/mash/ /tmp/met_1000.csv")
   q("no")
 }
+landseg <- as.character(argst[1])
+in_dir <- as.character(argst[2]) 
+time_template <- as.character(argst[3]) 
 
 time_template <- fread(wdm_template)
 #./make_wdm_template 1984 2024 met 1000
 
-met_ts <- timeseries_correction(met_ts, time_template, data_col)
+fexts <- s('PRC', 'PET', 'TMP', 'RAD', 'DPT', 'WND')
+for (i in length(fexts)) {
+  fext <- fexts[i]
+  fname <- paste0(in_dir,"/",landseg,".", fext)
+  met_ts <- data.table::fread(fname)
+  met_ts <- timeseries_correction(met_ts, time_template, data_col)
+  data.table::fwrite(met_ts, fname)
+}
+
