c6e08d6a939fcf807fd55d1395b7445e0b6b1b7e
Robert W. Burgholzer <rburghol@vt.edu>
2023-11-02 14:17:33 +0000

    saved new fn file

--- R/met_plots.R
+++ R/met_plots.R
@@ -1,7 +1,15 @@
-library(sqldf)
+basepath="/var/www/R"
+source("/var/www/R/config.R")
+suppressPackageStartupMessages(library(lubridate))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(sqldf))
+
 
 nldas_root=Sys.getenv(c('NLDAS_ROOT'))[1]
 nldas_url_base <- paste0(ext_url_base,'/met/out/lseg_csv')
+source(paste0(nldas_root,"/R/nldas_feature_dataset_prop.R"))
+ds <- RomDataSource$new(site, rest_uname)
+ds$get_token(rest_pw)
 
 argst <- commandArgs(trailingOnly = T)
 if (length(argst) < 2) {
@@ -14,10 +22,19 @@ landseg = argst[1]
 dataset = argst[2]
 landseg_ftype = argst[3]
 model_version_code = argst[4]
+if (length(argst) > 4) {
+  # store as a run scenario?
+  as_scen = argst[5]
+} else {
+  as_scen = 0
+}
 
-filedir <- paste(nldas_root, dataset, sep="/")
+outdir <- paste0(nldas_root,"/out/lseg_csv/",dataset,"/images/")
+outurl <- paste0(ext_url_base,"/met/out/lseg_csv/",dataset,"/images/")
+
+filedir <- paste(nldas_root,"out/lseg_csv", dataset, sep="/")
 scales <- list("PRC"=3.0, "PET" = 0.25)
-nldas_data <- nldas_feature_dataset_prop(ds, landseg, 'landunit',landseg_ftype, 'object')
+nldas_dataset <- nldas_feature_dataset_prop(ds, landseg, 'landunit', landseg_ftype, model_version_code, dataset, as_scen)
 
 for (comp in c('PRC', 'PET')) {
   filename <- paste(landseg,comp,sep=".")
@@ -26,12 +43,12 @@ for (comp in c('PRC', 'PET')) {
   names(dat) <- c("year", "month", "day", "hour", "tsvalue")
   # open the plot file
   dat <- sqldf("select year, month, day, sum(tsvalue) as tsvalue from dat group by year, month, day")
-  filename <- paste0(nldas_root,"/met/out/lseg_csv/",dataset,"/",landseg,"_rollingAVG_met.png")
+  filename <- paste0(outdir,landseg,"_annual_", comp,".png")
   png(filename)
   # render the plot
   boxplot(as.numeric(dat$tsvalue) ~ dat$year, ylim=c(0,as.numeric(scales[comp]) ))
   dev.off()
-  fileurl <- paste0(nldas_url_base,"/",dataset,"/",landseg,"_annual_", comp)
+  fileurl <- paste0(outurl,landseg,"_annual_", comp, ".png")
   ydat <- sqldf("select year, min(tsvalue), max(tsvalue), sum(tsvalue) as tsvalue from dat group by year")
   message(paste("Saving image file to:", filename, "URL:", fileurl))
   img_file <- RomProperty$new(
@@ -39,8 +56,8 @@ for (comp in c('PRC', 'PET')) {
     list(
       entity_type='dh_properties',
       propname=paste0('fig_annual_',comp),
-      varkey=img_file,
-      featureid=nldas_data$pid
+      varkey = 'dh_image_file',
+      featureid=nldas_dataset$pid
     ),
     TRUE
   )
