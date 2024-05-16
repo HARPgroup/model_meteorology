ba8508ff527cb44fd564289fb26f69c36b50adcc
Brogan <Connor.Brogan@deq.virginia.gov>
2024-05-16 10:49:08 -0400

    Added code to write raster to alpha database as a table. Need to get other identifier info in with it

--- prism/PRISMDownload.sh
+++ prism/PRISMDownload.sh
@@ -6,23 +6,16 @@ YYYY=$1
 #The second input is the extent (a vector mask) by which the imported precip rasters should be clipped
 maskExtent=$2
 
-#A simple array that has the number of days in each month for the target year
-days=(31 1 31 30 31 30 31 31 30 31 30 31)
+#A simple array that will have the number of days in each month for the target year
+declare -a days
 
-#Evaluate if $YYYY is a leap year e.g. either divisible by 4 or 400, but not 100 inherently
-a=$(( YYYY % 4 ))
-b=$(( YYYY % 100 ))
-c=$(( YYYY % 400 ))
-
-#Check for leap year: is $a divisible by 4 and $b was not divisible by 100? or is $c divisible by 400?
-if [ $a -eq 0 -a $b -ne 0 -o $c -eq 0 ]
-then
-#If leap year, set February days to 29
-days[1]=29
-else
-#Otherwise set to 28
-days[1]=28
-fi
+#Evaluate if $YYYY is a leap year e.g. either divisible by 4 or 400, but not 100 inherently. 
+#Below, we use the -d option to coerce date to get the maximum day available in each month of 
+#the input year. This is done in each step of the loop using the date coercion at the beginning 
+#e.g. the $y/$m/1 sets the date initially to the date $m/01/%y. From there, we addd a month and subtract a day.
+for m in {1..12}; do
+  days[$m - 1]=`date -d "$m/1 + 1 month - 1 day" "+%d"`
+done
 
 #Now, download a raster for each day of each month from the PRISM webpage
 for i in {0..11}
@@ -40,12 +33,17 @@ for i in {0..11}
 		#Clipping the raster: Use gdalwarp to crop to the cutline maskExtent.csv, which is a csv of the CBP regions 
 		gdalwarp -cutline $maskExtent -crop_to_cutline PRISM-conus-4326.bil PRISM-CBP-4326-$YYYY$MM$DD.bil
 		
-		#Bring into database
+		#Create sql file that will add the raster (-a for amend) into the target table
+		raster2pgsql -a -t 1000x1000 PRISM-CBP-4326-$YYYY$MM$DD.bil tmp_prism > tmp_prism-test.sql
 		
+		#Execute sql file to bring raster into database (alpha)
+		psql -h dbase2 -f "tmp_prism-test.sql" -d drupal.alpha
 		
+		#Remove all create files for next loop
 		rm $YYYY$MM$DD
 		rm PRISM_ppt_stable_*
 		rm PRISM-conus-4326*
+		rm tmp_prism-test.sql
 	done
 done
 
