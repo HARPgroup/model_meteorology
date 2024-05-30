ffbfc4173abcb1fbd179adb36abe62fd37fb88c8
Brogan <Connor.Brogan@deq.virginia.gov>
2024-05-30 13:35:23 -0400

    Change to daymetDownload to allow either individual day or full yeaar download

--- daymet/daymetDownload_Step1.sh
+++ daymet/daymetDownload_Step1.sh
@@ -5,11 +5,15 @@
 #Only full years are downloaded but LEAP YEARS REMOVE DEC 31st!
 
 #Required inputs are:
-#$1 = Start year
-#$2 = End year, may equal start Year
-#3 = File to mask extent to be used for bounding box of downloads
-startYear=$1
-endYear=$2
+#$1 = Date of interest
+#$2 = Output directory
+dateIn=$1
+output_dir=$2
+
+#Get the year associated with the date
+YYYY=`date -d "${dateIn}" "+%Y"`
+#Get the julian day associated with the date
+jday=`date -d "${dateIn}" "+%j"`
 
 #Region - NA is used for conus. The complete list of regions is: na (North America), hi(Hawaii), pr(Puerto Rico)
 region="na"
@@ -36,70 +40,77 @@ declare -A bbox=(
 )
 
 
-
-for ((YYYY=startYear;YYYY<=endYear;YYYY++)); do
+#In case of need of loop for several years, see below commented out loop:
+#for ((YYYY=startYear;YYYY<=endYear;YYYY++)); do
 echo $YYYY
-  #Allow for future temperature or other met data downloads.
-  #For now, par = $var = precip
-	for par in $var; do
-	  echo $par
-		
-		#Output file named. This will be the netCDF downloaded from daymet
-		daymetOriginal=${config["dataset"]}${par}_${YYYY}.nc
-		
-		#Evaluate if the year is a leap year. daymet uses 365 day years and on leap years December 31st will be missing:
-		#Evaluate if $YYYY is a leap year e.g. either divisible by 4 or 400, but not 100 inherently. 
-		#Below, we use the -d option to coerce date to get the maximum day available in each month of 
-		#the input year. This is done in each step of the loop using the date coercion at the beginning 
-		#e.g. the $y/$m/1 sets the date initially to the date $m/01/%y. From there, we addd a month and subtract a day.
-		if [ `date -d "${YYYY}/02/1 + 1 month - 1 day" "+%d"` -eq 29 ]; then
-			#For leap years, end query at december 30th
-			wget -O $daymetOriginal "https://thredds.daac.ornl.gov/thredds/ncss/grid/ornldaac/2129/daymet_v4_daily_${region}_${par}_${YYYY}.nc?var=lat&var=lon&var=${par}&north=${bbox["north"]}&west=${bbox["west"]}&east=${bbox["east"]}&south=${bbox["south"]}&horizStride=1&time_start=${YYYY}-01-01T00:00:00Z&time_end=${YYYY}-12-30T23:59:59Z&timeStride=1&accept=netcdf"
-		else
-			#For non-leap years, query january 1st through december 31st
-			wget -O daymet_precip_prcp_2019_fullDay.nc "https://thredds.daac.ornl.gov/thredds/ncss/grid/ornldaac/2129/daymet_v4_daily_${region}_${par}_${YYYY}.nc?var=lat&var=lon&var=${par}&north=${bbox["north"]}&west=${bbox["west"]}&east=${bbox["east"]}&south=${bbox["south"]}&horizStride=1&time_start=${YYYY}-01-01T00:00:00Z&time_end=${YYYY}-12-31T23:59:59Z&timeStride=1&accept=netcdf"
-		fi
-		
-		#Identify how many bands there are in the raster. For a full year download, 
-		#this should be 365. However, we should evaluate this programmatically to 
-		#be sure. Below, we get only the band numbers from gdal info by first 
-		#searching in the pattern Band XX and then getting only the maximum number 
-		#via a reverse sort
-		numBands=`gdalinfo NETCDF:"${daymetOriginal}":prcp -nomd -norat | grep -oP "Band [0-9]* " | grep -oP "[0-9]*" | sort -nr | head -n1`
-		
-		#Find the dates associated with the bands, pulling the NETCDF dim time 
-		#values from the metadata. This comes as 
-		#NETCDF_DIM_time_VALUES={DATE1,DATE2,...,DATEN}
-		#so we use pattern matching to find a repeating group using ()
-		bandDates=`gdalinfo NETCDF:"${daymetOriginal}":prcp | grep -oP "NETCDF_DIM_time_VALUES.*" | grep -oP "([0-9]*\.?[0-9]*,?[0-9]*\.?[0-9]*)+"`
-		
-		#Origin for dates:
-		dateOrigin=`gdalinfo NETCDF:"${daymetOriginal}":prcp | grep -oP "time#units=days since .*" | grep -oP "[0-9]+.*"`
-		#GET ONLY DATE: ASSUMES 00:00:00 UTC!
-		dateOriginNoTime=`gdalinfo NETCDF:"${daymetOriginal}":prcp | grep -oP "time#units=days since .*" | grep -oP "[0-9]+.*" | grep -oP "[0-9]+-[0-9]+-[0-9]+"`
-		
-		for (( i=1 ; DDIt<=$numBands ; i++ ))
-		  do
-		  #Write data to the appropriate folder:
+#Allow for future temperature or other met data downloads.
+#For now, par = $var = precip
+for par in $var; do
+  echo $par
+	#Output file named for the full year archive. This will be the netCDF downloaded from daymet
+	daymetOriginal=${config["dataset"]}${par}_${YYYY}.nc
+  
+  #Evalute the user input configuration. Is the full year to be downloaded?
+  #Is forcing on?
+  #If the full year archive is not available, download and extract all days.
+  #If it is available and forcing is on OR the request day is unavailable, extract
+  #from the full year archive.
+  if [ ! -f "${output_dir}/${YYYY}/${daymetOriginal}" ]; then
+    #Download full year archive and extract all days:
+    
+    #Evaluate if the year is a leap year. daymet uses 365 day years and on leap years December 31st will be missing:
+	  #Evaluate if $YYYY is a leap year e.g. either divisible by 4 or 400, but not 100 inherently. 
+	  #Below, we use the -d option to coerce date to get the maximum day available in each month of 
+	  #the input year. This is done in each step of the loop using the date coercion at the beginning 
+	  #e.g. the $y/$m/1 sets the date initially to the date $m/01/%y. From there, we addd a month and subtract a day.
+	  if [ `date -d "${YYYY}/02/1 + 1 month - 1 day" "+%d"` -eq 29 ]; then
+	  	#For leap years, end query at december 30th
+	  	wget -O $daymetOriginal "https://thredds.daac.ornl.gov/thredds/ncss/grid/ornldaac/2129/daymet_v4_daily_${region}_${par}_${YYYY}.nc?var=lat&var=lon&var=${par}&north=${bbox["north"]}&west=${bbox["west"]}&east=${bbox["east"]}&south=${bbox["south"]}&horizStride=1&time_start=${YYYY}-01-01T00:00:00Z&time_end=${YYYY}-12-30T23:59:59Z&timeStride=1&accept=netcdf"
+	  else
+	  	#For non-leap years, query january 1st through december 31st
+	  	wget -O $daymetOriginal "https://thredds.daac.ornl.gov/thredds/ncss/grid/ornldaac/2129/daymet_v4_daily_${region}_${par}_${YYYY}.nc?var=lat&var=lon&var=${par}&north=${bbox["north"]}&west=${bbox["west"]}&east=${bbox["east"]}&south=${bbox["south"]}&horizStride=1&time_start=${YYYY}-01-01T00:00:00Z&time_end=${YYYY}-12-31T23:59:59Z&timeStride=1&accept=netcdf"
+	  fi
+	  
+	  #Identify how many bands there are in the raster. For a full year download, 
+	  #this should be 365. However, we should evaluate this programmatically to 
+	  #be sure. Below, we get only the band numbers from gdal info by first 
+	  #searching in the pattern Band XX and then getting only the maximum number 
+	  #via a reverse sort
+	  numBands=`gdalinfo NETCDF:"${daymetOriginal}":prcp -nomd -norat | grep -oP "Band [0-9]* " | grep -oP "[0-9]*" | sort -nr | head -n1`
+	  
+	  #Find the dates associated with the bands, pulling the NETCDF dim time 
+	  #values from the metadata. This comes as 
+	  #NETCDF_DIM_time_VALUES={DATE1,DATE2,...,DATEN}
+	  #so we use pattern matching to find a repeating group using ()
+	  bandDates=`gdalinfo NETCDF:"${daymetOriginal}":prcp | grep -oP "NETCDF_DIM_time_VALUES.*" | grep -oP "([0-9]*\.?[0-9]*,?[0-9]*\.?[0-9]*)+"`
+	  
+	  #Origin for dates:
+	  dateOrigin=`gdalinfo NETCDF:"${daymetOriginal}":prcp | grep -oP "time#units=days since .*" | grep -oP "[0-9]+.*"`
+	  #GET ONLY DATE: ASSUMES 00:00:00 UTC!
+	  dateOriginNoTime=`gdalinfo NETCDF:"${daymetOriginal}":prcp | grep -oP "time#units=days since .*" | grep -oP "[0-9]+.*" | grep -oP "[0-9]+-[0-9]+-[0-9]+"`
+	  
+	  for (( i=1 ; DDIt<=$numBands ; i++ ))
+	    do
+	    #Write data to the appropriate folder:
       #Check if base directory for the julian day exists. If not, create it:
-      if [ ! -d "$base_dir/$yr/$jday" ]; then
+      if [ ! -d "${output_dir}/${YYYY}/${i}" ]; then
         #Using -p option, create the Parent year directory if it has not yet 
         #been created
-        mkdir -p "$base_dir/$yr/$jday" 
+        mkdir -p "${output_dir}/${YYYY}/${i}" 
       fi
       #Set the source directory for this file:
-      src_dir="$base_dir/$yr/$jday"
-		  
-		  #Get the day associated with the current raster based on $bandDates above.
-		  #Use awk to search in a comma separated (-F ',') file for line $i.
-		  #Note the use of single quotes! Remove the decimal to floor down to an 
-		  #integer
-		  bandDatei=`echo $bandDates | awk -F ',' '{print $'$i'}' | grep -oP "^[0-9]+"`
-		  #Dates are 1 day behind, add 1
-		  bandDatei=$(( $bandDatei + 1))
-		  
-		  #bandDatei is the days since $dateOriginNoTime (ASSUMES dateOriginN)
-		  checkMetaDate=`date -d "${dateOriginNoTime} +$(( ${bandDatei} -1 ))days" +%Y-%m-%d`
+      src_dir="${output_dir}/${YYYY}/${i}"
+	    
+	    #Get the day associated with the current raster based on $bandDates above.
+	    #Use awk to search in a comma separated (-F ',') file for line $i.
+	    #Note the use of single quotes! Remove the decimal to floor down to an 
+	    #integer
+	    bandDatei=`echo $bandDates | awk -F ',' '{print $'$i'}' | grep -oP "^[0-9]+"`
+	    #Dates are 1 day behind, add 1
+	    bandDatei=$(( $bandDatei + 1))
+	    
+	    #bandDatei is the days since $dateOriginNoTime (ASSUMES dateOriginN)
+	    checkMetaDate=`date -d "${dateOriginNoTime} +$(( ${bandDatei} -1 ))days" +%Y-%m-%d`
       #Based on julian day (estimated based on assumption of complete dataset 
       #and the raster band number)
       checkJulianDate=`date -d "${YYYY}-01-01 +$(( ${i} -1 ))days" +%Y-%m-%d`
@@ -111,13 +122,79 @@ echo $YYYY
       finalTiff="ORIGINAL_${config["dataset"]}${checkJulianDate}${config["ext"]}"
       
       #Send this band to its own raster
-		  gdal_translate -b $i NETCDF:"${daymetOriginal}":prcp -of "gtiff" $src_dir/$finalTiff
-		  
-		#End band for loop
-		done
-	#End var for loop
-	done
-#End YYYY for loop
+	    gdal_translate -b $i NETCDF:"${daymetOriginal}":prcp -of "gtiff" $src_dir/$finalTiff
+	    
+	  #End band for loop
+	  done
+    
+  else
+    #Annual archive already exists
+    annualArchive=${output_dir}/${YYYY}/${daymetOriginal}
+    #Does the daily gtiff already exist?
+    checkFile="ORIGINAL_${config["dataset"]}${dateIn}${config["ext"]}"
+    if [ ! -f "${output_dir}/${YYYY}/${jday}/${checkFile}" ] || [ $forcing 1 -eq ]; then
+      #Indivudal day either does not exist or forcing is on. Extract day from 
+      #Rest services:
+      #Output file named. This will be the netCDF downloaded from daymet
+	    daymetOriginal=${config["dataset"]}${par}_${dateIn}.nc
+	
+	    #Evaluate if the year is a leap year. daymet uses 365 day years and on leap years December 31st will be missing:
+	    #Evaluate if $YYYY is a leap year e.g. either divisible by 4 or 400, but not 100 inherently. 
+	    #Below, we use the -d option to coerce date to get the maximum day available in each month of 
+	    #the input year. This is done in each step of the loop using the date coercion at the beginning 
+	    #e.g. the $y/$m/1 sets the date initially to the date $m/01/%y. From there, we addd a month and subtract a day.
+	    if [ `date -d "${YYYY}/02/1 + 1 month - 1 day" "+%d"` -eq 29 ]; then
+	    	if [ "${dateIn}" = "${YYYY}-12-31" ];then
+	    	#For leap years, end query at december 30th
+	    	  echo "DECEMBER 31st CANNOT BE DOWNLOADED DUE TO DAYMET YEAR LIMITATIONS"
+	    	  exit
+	    	fi
+	    fi
+	    #For non-leap years, query January 1st through december 31st
+	    wget -O $daymetOriginal "https://thredds.daac.ornl.gov/thredds/ncss/grid/ornldaac/2129/daymet_v4_daily_${region}_${par}_${YYYY}.nc?var=lat&var=lon&var=${par}&north=${bbox["north"]}&west=${bbox["west"]}&east=${bbox["east"]}&south=${bbox["south"]}&horizStride=1&time_start=${dateIn}T00:00:00Z&time_end=${dateIn}T23:59:59Z&timeStride=1&accept=netcdf"
+	    
+	    #Find the dates associated with the bands, pulling the NETCDF dim time 
+	    #values from the metadata. This comes as 
+	    #NETCDF_DIM_time_VALUES={DATE1,DATE2,...,DATEN}
+	    #so we use pattern matching to find a repeating group using ()
+	    bandDates=`gdalinfo NETCDF:"${daymetOriginal}":prcp | grep -oP "NETCDF_DIM_time_VALUES.*" | grep -oP "([0-9]*\.?[0-9]*,?[0-9]*\.?[0-9]*)+"`
+	    
+	    #Origin for dates:
+	    dateOrigin=`gdalinfo NETCDF:"${daymetOriginal}":prcp | grep -oP "time#units=days since .*" | grep -oP "[0-9]+.*"`
+	    #GET ONLY DATE: ASSUMES 00:00:00 UTC!
+	    dateOriginNoTime=`gdalinfo NETCDF:"${daymetOriginal}":prcp | grep -oP "time#units=days since .*" | grep -oP "[0-9]+.*" | grep -oP "[0-9]+-[0-9]+-[0-9]+"`
+	    
+      #Write data to the appropriate folder:
+      #Check if base directory for the julian day exists. If not, create it:
+      if [ ! -d "$output_dir/$YYYY/$jday" ]; then
+        #Using -p option, create the Parent year directory if it has not yet 
+        #been created
+        mkdir -p "$output_dir/$YYYY/$jday" 
+      fi
+      #Set the source directory for this file:
+      src_dir="$output_dir/$YYYY/$jday"
+      
+      #Get the day associated with the current raster based on $bandDates above.
+      #Remove the decimal to floor down to an integer
+      bandDates=`echo $bandDates | grep -oP "^[0-9]+"`
+      #Dates are 1 day behind, add 1
+      bandDates=$(( $bandDates + 1))
+	      
+      #$bandDates is the days since $dateOriginNoTime (ASSUMES dateOriginN)
+      checkMetaDate=`date -d "${dateOriginNoTime} +$(( ${bandDates} -1 ))days" +%Y-%m-%d`
+      
+      if [ "$checkMetaDate" != "$dateIn" ]; then
+        echo "WARNING: Date in Meta data does NOT match the expected date for band 1!"
+      fi
+        
+      finalTiff="ORIGINAL_${config["dataset"]}${dateIn}${config["ext"]}"
+        
+      #Send this band to its own raster
+      gdal_translate -b 1 NETCDF:"${daymetOriginal}":prcp -of "gtiff" $src_dir/$finalTiff
+    fi
+  fi
+	
+#End var for loop
 done
 
 
