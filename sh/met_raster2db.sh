03bad7eaa96095c1208e2ec9d52af8ecf82c5e3e
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2024-05-30 17:53:09 -0400

    bad quote actually

--- sh/met_raster2db.sh
+++ sh/met_raster2db.sh
@@ -59,7 +59,7 @@ if [ "$force" == "1" ]; then
 			select hydroid from dh_feature where hydrocode = '${extent_hydrocode}'
 		)
 		and tstime = $tstime 
-		and tsendtime = '$tsendtime
+		and tsendtime = $tsendtime
 		;" | psql -h $db_host -d $db_name 
 fi
 
