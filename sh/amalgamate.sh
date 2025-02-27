1e8aa81862c67efc1a082e9a7b1631efeedf67cb
connor.brogan@deq.virginia.gov <connor.brogan@deq.virginia.gov>
2025-02-27 15:55:07 -0500

    Added a reclass to prevent conflicts between no data values

--- sh/amalgamate.sh
+++ sh/amalgamate.sh
@@ -15,8 +15,11 @@
 #14 = DELETE_TF = Should existing records be deleted? Should be TRUE or FALSE
 #15-16 = db_host, db_name = The host, name of the database in which data is and will be stored
 
-#We set up a local name reference to the config array passed in by the user to easily access its value
-#CAUTION: Changes to confignr likely impact the original array passed by user!
+#This script takes the key raster (varid of best data set in each cell) and sets one varid to NULL. It then takes the resampled (to target resolution) raw precip
+#data and fills in the NULLs via a union such that we derive a product that has varids in the non-target cells and precip data in the cells that were of the target varid
+#This is then added to the dh_timeseries_weather and can be called for further manipulation. To allow us to use a loop for calling this script, the base raster is either the product of the
+#previous loop or the key raster and is controlled by the first statement in WITH. A temporary table is used to store the intermediate before a delete+insert adds It
+#to the database. Note that the final raster is reclassified to remove any negative values and instead replace them with a consistent no data value.
 if [ $# -lt 16 ]; then
   echo "Use: amalgamate.sh TS_START_IN TS_END_IN RESAMPLE_VARKEY AMALGAMATE_SCENARIO AMALGAMATE_VARKEY RATINGS_VARKEY COVERAGE_BUNDLE COVERAGE_FTYPE SCENARIO_NAME EXTENT_HYDROCODE EXTENT_BUNDLE EXTENT_FTYPE AMALGAMATE_SQL_FILE DELETE_TF db_host db_name"
   exit
@@ -155,7 +158,7 @@ WHERE varid = :ratings     \n
      \n
 INSERT INTO dh_timeseries_weather (tstime,tsendtime, featureid, entity_type, rast, bbox, varid)     \n
 SELECT :tsstartin,:tsendin,:scenariopid,'dh_properties',     \n
-	fr.rast,     \n
+	ST_Reclass(fr.rast,1,'-9999-0:9999,0-9999:0-9999','32BF',9999), \n
 	ST_ConvexHull(fr.rast),     \n
 	:ratings     \n
 FROM tmp_amalgamate as fr     \n
@@ -174,4 +177,4 @@ echo "Writing sql insert to $AMALGAMATE_SQL_FILE"
 #Delete previous dh_timeseries entries
 echo $amalSQL
 echo -e $amalSQL > $AMALGAMATE_SQL_FILE 
-#Check if TRUE FALSE works and check SCENARIO TO RANK to ensure proper format for for loop e.g. are there parentheses?
+cat $AMALGAMATE_SQL_FILE | psql -h $db_host $db_name
