1cac2dafe1ec2688c9518bf84d2e1682438d1174
Connor Brogan <connor.brogan@deq.virginia.gov>
2026-05-05 14:01:57 -0400

    Added null value input to met_raster2db

--- sh/met_raster2db.sh
+++ sh/met_raster2db.sh
@@ -4,16 +4,19 @@
 #1 = datasource = precip data name used in temporary file generation e.g. "prism"
 #2 = finalTiff = File path of the raster to import into dbase
 #3 = tstime = string in form of YYYY-MM-DD HH
-#4 = tsElapse/dt = How much time in seconds is this data representative for? E.g. for daily data, this is 86400
+#4 = tsendtime = string in form of YYYY-MM-DD HH
 #5 = entity_type = entity_type of desired db feature
 #6 = varkey = The key for the variable definition in dh_variabledefinition of interest e.g. daymet_precip_raster for daymet data
-#7 = extent hydrocode
-#8 = db_name
+#7 = extent_hydrocode = Hydrocode of base feature
+#8 = tile_size = How many pixels in each square tile?
+#9 = db_name = data base name on host
+#10 = db_host = host database
+#11 = no_data_value = false or double value to represent the no data pixel value. If false, will use PSQL default algorithim
+#12 = force = Should data be deleted and reimported?
 
-#We set up a local name reference to the config array passed in by the user to easily access its value
-#CAUTION: Changes to confignr likely impact the original array passed by user!
+
 if [ $# -lt 11 ]; then
-  echo "Use: met_raster2dn.sh datasource finalTiff tstime tsendtime entity_type varkey extent_hydrocode tile_size db_name db_host force"
+  echo "Use: met_raster2dn.sh datasource finalTiff tstime tsendtime entity_type varkey extent_hydrocode tile_size db_name db_host no_data_value force"
   exit
 fi
 datasource=$1
@@ -26,16 +29,24 @@ extent_hydrocode=$7
 tile_size=$8
 db_name=$9
 db_host=${10}
+no_data_value=${11}
+#If no_data_value is set to false, assume the default raster2pgsql algorithim is sufficient.
+#Otherwise, manually set no data value on call
+if [ "$no_data_value" == 'false' ]; then
+  no_data_option=""
+else
+  no_data_option="-N ${no_data_value} "
+fi
 force=0
-if [ $# -gt 10 ]; then
-  force=${11}
+if [ $# -gt 11 ]; then
+  force=${12}
 fi
 echo "Creating sql file to import raster..."
 #Create sql file that will add the raster (-a for amend or -d for drop and recreate) into the target table
 #The -t option tiles the raster for easier loading
 tmp_sql_file=tmp_${datasource}-${tstime}-test.sql
 tmp_tbl_name="tmp_${datasource}_${tstime}"
-raster2pgsql -d -t "${tile_size}x${tile_size}" $finalTiff $tmp_tbl_name > $tmp_sql_file
+rast_evalu="raster2pgsql -d -t ${tile_size}x${tile_size} ${no_data_option}${finalTiff} ${tmp_tbl_name} > ${tmp_sql_file}"
 
 echo "Sending raster to db..."
 #Execute sql file to bring rasters into database (alpha)
